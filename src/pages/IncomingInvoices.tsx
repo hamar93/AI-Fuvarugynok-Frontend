c581fe90f2e65a0faa9d7723483fcc4f9f477686
hamar93 <hamargabor1993@gmail.com>
2025-06-16 18:33:03 +0200

    Add files via upload

---
+++ src/pages/IncomingInvoices.tsx
@@ -0,0 +1,4 @@
+import React from 'react';
+export default function IncomingInvoices() {
+  return <div>Beérkező számlák oldal – ide jön a tartalom.</div>;
+}
---
+++ src/pages/OutgoingInvoices.tsx
@@ -0,0 +1,4 @@
+import React from 'react';
+export default function OutgoingInvoices() {
+  return <div>Kimenő számlák oldal – ide jön a tartalom.</div>;
+}
